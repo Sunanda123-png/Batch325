9dd4b893cbb9ac4adc30932d3ce43ed756a152a9
Sunanda123-png <sunanda02@gmail.com>
2021-07-21 11:53:33 +0530

    add day1 programm

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+class HelloWorld
+{
+	public static void main(String args[])
+	{
+		while(true)
+		{
+			System.out.println("Hello world");
+
+			try
+			{
+				Thread.sleep(10000);
+
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+	}
+}
